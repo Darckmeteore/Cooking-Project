481a28d78d7b45110daebf7eb953251096ebc764
Debzou <david.petit36@gmail.com>
2019-11-12 14:29:13 +0100

    camera

--- app/src/app/meals/meals.page.ts
+++ app/src/app/meals/meals.page.ts
@@ -4,7 +4,7 @@ import { LoadingController, NavController, Platform } from '@ionic/angular';
 import { RestService } from '../app-rest-service.service';
 // ionic cordova plugin add cordova-plugin-camera
 // npm install @ionic-native/camera
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+//import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 
 @Component({
@@ -19,7 +19,7 @@ export class MealsPage {
   devWidth: any;
   
 
-  constructor(public restapi: RestService, public loadingController: LoadingController, public navController : NavController, public plateform: Platform, private camera: Camera) {
+  constructor(public restapi: RestService, public loadingController: LoadingController, public navController : NavController, public plateform: Platform) {
 
     this.api = restapi;
     this.devWidth = this.plateform.width();
@@ -55,19 +55,3 @@ export class MealsPage {
   }
   
 }
-
-//CAMERAA 
-const options: CameraOptions = {
-  quality: 100,
-  destinationType: this.camera.DestinationType.FILE_URI,
-  encodingType: this.camera.EncodingType.JPEG,
-  mediaType: this.camera.MediaType.PICTURE
-}
-
-this.camera.getPicture(options).then((imageData) => {
- // imageData is either a base64 encoded string or a file URI
- // If it's base64 (DATA_URL):
- let base64Image = 'data:image/jpeg;base64,' + imageData;
-}, (err) => {
- // Handle error
-});
