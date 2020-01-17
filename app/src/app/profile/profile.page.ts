25db3879bcdd9ba1cdda235d146180bb3dbdc126
Debzou <david.petit36@gmail.com>
2020-01-18 00:19:45 +0100

    end

--- app/src/app/profile/profile.page.ts
+++ app/src/app/profile/profile.page.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {LoadingController} from "@ionic/angular";
 import {ActivatedRoute} from "@angular/router";
+import { GlobalService } from '../global.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private global : GlobalService) { }
 
   ngOnInit() {
   }
